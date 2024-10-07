7276f7b58fad6d183818244bddaf865cdca02988
Antony Naumovic <40827853+antonynaumovic@users.noreply.github.com>
2024-10-07 14:47:38 +0100

    Invert Fix

--- __init__.py
+++ __init__.py
@@ -10,6 +10,10 @@ from mathutils.geometry import (
                 distance_point_to_plane,
                 normal)
 
+import array
+from . import lib
+
+
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
@@ -27,15 +31,15 @@ bl_info = {
     "name" : "SierraRenamer",
     "author" : "Ant",
     "description" : "",
-    "blender" : (4, 0, 0),
-    "version" : (0, 0, 6),
+    "blender" : (4, 2, 2),
+    "version" : (0, 0, 8),
     "location" : "",
     "warning" : "",
     "category" : "Mesh"
 }
 
 
-class MySettings(bpy.types.PropertyGroup):
+class SierraSettings(bpy.types.PropertyGroup):
 
     string_prefix : bpy.props.StringProperty(
         name="Prefix",
@@ -51,6 +55,13 @@ class MySettings(bpy.types.PropertyGroup):
         name="Leading 0s",
         description="Amount Of Leading 0s",
         default=1
+    )
+    float_concaveTolerance : bpy.props.FloatProperty(
+        name="Concave Tolerance",
+        description="Tolerance to find concave",
+        default=0,
+        min=0,
+        max=1,
     )
 
     enum_suffixAction: bpy.props.EnumProperty(
@@ -131,6 +142,7 @@ class Renamer_PT_Panel(bpy.types.Panel):
 
         toolbox = layout.box()
         toolbox.label(text="Tools")
+        toolbox.prop(mytool, "float_concaveTolerance", text="Tolerance", slider=True, expand=True, index=0)
         toolbox.operator("object.showconcave", text="Show Concave")
 
 
@@ -301,29 +313,107 @@ class ShowConcave_OT_Operator(bpy.types.Operator):
     def execute(self, context):
         OBs = bpy.context.selected_objects
         mode = context.active_object.mode
+        scene = context.scene
+        mytool = scene.my_tool
+        TOL = mytool.float_concaveTolerance
         for ob in OBs:
-            bpy.ops.object.mode_set(mode='EDIT')
-            mesh = ob.data
 
-            TOL = 0.0001
+            tolerance = TOL  # increase to something like .01 or .1 to ignore small concavities
 
-            # select None
-            bpy.ops.mesh.select_all(action='DESELECT')
-            bm = bmesh.from_edit_mesh(mesh)
-            ngons = [f for f in bm.faces if len(f.verts) > 3]
+            bpy.ops.object.mode_set(mode = 'EDIT') 
+            bpy.ops.mesh.select_mode(type="FACE")
+            bpy.ops.mesh.select_all(action = 'DESELECT')
+            bpy.ops.object.mode_set(mode = 'OBJECT')
 
-            for ngon in ngons:
-                # define a plane from first 3 points
-                co = ngon.verts[0].co
-                norm = normal([v.co for v in ngon.verts[:3]])
+            me = bpy.context.active_object.data
+            me.calc_loop_triangles()
+            poly_i_to_tris = {}
+            for tri in me.loop_triangles:
+                poly_i_to_tris.setdefault(tri.polygon_index, []).append(tri)
+            polys = {}
+            for i, tris in poly_i_to_tris.items():
+                try:
+                    t1, t2 = tris
+                except ValueError:
+                    continue  # not a quad
+                dist = (t1.center - t2.center).length
+                tolerance *= dist  # makes tolerance relative
+                ray_len = dist/2  # making sure to not overshoot, but I don't think it would be possible
+                test1 = t1.center + t1.normal*ray_len
+                test2 = t2.center + t2.normal*ray_len
+                test_dist = (test1 - test2).length
+                if test_dist < dist - tolerance:
+                    # concave
+                    me.polygons[i].select = True
+            bpy.ops.object.mode_set(mode = 'EDIT')
+            bpy.ops.mesh.normals_make_consistent(inside=True)                     
+            bm = bmesh.from_edit_mesh(me)
+            for face in bm.faces:
+                face.select_set(False)
+                for loop in face.loops:
+                    if not loop.is_convex:
+                        face.select_set(True)
+                        bpy.ops.mesh.normals_make_consistent(inside=True)
+                        break
+            
 
-                ngon.select =  not all(
-                    [(distance_point_to_plane(v.co, co, norm)) < TOL
-                    for v in ngon.verts[3:]])
-                if ngon.select: 
-                    bpy.ops.mesh.normals_make_consistent(inside=True)
+            # bpy.ops.object.mode_set(mode='EDIT')
+            # mesh = ob.data
 
-            bmesh.update_edit_mesh(mesh)
+            
+
+            # # select None
+            # bpy.ops.mesh.select_all(action='DESELECT')
+            # # bm = bmesh.from_edit_mesh(mesh)
+            # # ngons = [f for f in bm.faces if len(f.verts) > 3]
+
+            # ob = bpy.context.edit_object
+            # me = ob.data
+
+            # bm = bmesh.from_edit_mesh(me)
+            # bm.faces.active = None
+
+            # for face in bm.faces:
+            #     face.select_set(False)
+            #     for loop in face.loops:
+            #         if not loop.is_convex:
+            #             face.select_set(True)
+            #             break
+
+            # bmesh.update_edit_mesh(me)
+
+            # for ngon in ngons:
+            #     # define a plane from first 3 points
+            #     co = ngon.verts[0].co
+            #     norm = normal([v.co for v in ngon.verts[:3]])
+
+            #     ngon.select =  not all(
+            #         [(distance_point_to_plane(v.co, co, norm)) < TOL
+            #         for v in ngon.verts[3:]])
+            #     if ngon.select: 
+            #         bpy.ops.mesh.normals_make_consistent(inside=True)
+
+            # for ngon in ngons:
+            # # define a plane from first 3 points
+            #     co = ngon.verts[0].co
+            #     norm = normal([v.co for v in ngon.verts[:3]])
+
+            #     ngon.select =  lib.face_is_distorted(ngon, TOL)
+            #     if ngon.select: 
+            #         bpy.ops.mesh.normals_make_consistent(inside=True)
+
+            # for face in bm.faces:
+            #     face.select_set(False)
+            #     for loop in face.loops:
+            #         if not loop.is_convex:
+            #             face.select_set(True)
+            #             bpy.ops.mesh.normals_make_consistent(inside=True)
+            #             break
+
+            # bm.free()
+
+
+            # bmesh.update_edit_mesh(mesh)
             bpy.ops.object.mode_set(mode='OBJECT')
         bpy.ops.object.mode_set(mode=mode)
         bpy.context.space_data.overlay.show_face_orientation = True
@@ -331,12 +421,12 @@ class ShowConcave_OT_Operator(bpy.types.Operator):
 
 
 
-classes = (MySettings, SierraRenamer_OT_Operator, Renamer_PT_Panel, SierraUV_PT_Panel, ShowConcave_OT_Operator, SierraStackUnstack_OT_Operator, SierraToggleUVLines_OT_Operator, SierraToggleCrease_OT_Operator)
+classes = (SierraSettings, SierraRenamer_OT_Operator, Renamer_PT_Panel, SierraUV_PT_Panel, ShowConcave_OT_Operator, SierraStackUnstack_OT_Operator, SierraToggleUVLines_OT_Operator, SierraToggleCrease_OT_Operator)
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    bpy.types.Scene.my_tool = bpy.props.PointerProperty(type=MySettings)
+    bpy.types.Scene.my_tool = bpy.props.PointerProperty(type=SierraSettings)
 
 def unregister():
     del bpy.types.Scene.my_tool
